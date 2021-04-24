5b3609cac838f554694ac84d1330342a47898150
root <root@ip-10-0-1-229.us-east-2.compute.internal>
2021-04-24 12:04:46 +0000

    function file created successfully

--- function.sh
+++ function.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 echo "Add first number "
-read $num1 | bc
+read "num1"
 echo "Add second number "
-read $num2 | bc
+read "num2"
 echo "What you want to do add,sub,mul,div "
-read $exprs
+read "exprs"
 
-if [[ "$exprs" == "add" ]]; then ans=`expr $num1 + $num2`
-elif [[ "$exprs" == "sub" ]]; then ans=`expr $num1 - $num2`
-elif [[ "$exprs" == "mul" ]]; then ans=`expr $num1 \* $num2`
-elif [[ "$exprs" == "div" ]]; then ans=`expr $num1 / $num2`
-else echo "we cant do anything"
+val1=$(( $num1 + $num2 ))
+val2=$(( $num1 - $num2 ))
+val3=$(( $num1 * $num2 ))
+val4=$(( $num1 - $num2 ))
+
+if [ "$exprs" = "add" ]
+then echo "$val1"
+elif [ "$exprs" = "sub" ]
+then echo "$val2"
+elif [ "$exprs" = "mul" ]
+then echo "$val3"
+elif [ "$exprs" = "div" ]
+then echo "$val4"
 fi
-echo "Your answer is $ans"
